31c9b94e92ca8bf1c072ed33d7c11c2f604e4c61
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2008-05-03 14:23:58 +0000

    [Gtk2] Add QTC_NEW_MOZILLA cmake option.

--- common/config_file.c
+++ common/config_file.c
@@ -1243,8 +1243,13 @@ static void defaultSettings(Options *opts)
     opts->gtkButtonOrder=false;
 #endif
 #ifndef __cplusplus
+#ifdef QTC_NEW_MOZILLA
+    opts->newFirefox=true;
+    opts->newThunderbird=true;
+#else
     opts->newFirefox=false;
     opts->newThunderbird=false;
+#endif
 #endif
 #ifdef __cplusplus
     opts->titlebarAppearance=APPEARANCE_GRADIENT;
