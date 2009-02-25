2753cf9f8f2db4bf25be85792b56d60ac8a86906
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2009-02-25 20:53:03 +0000

    Reduce gradient code complexity - makes predefined gradients work in the same vein as custom gradients.

--- config/qtcurveconfig.h
+++ config/qtcurveconfig.h
@@ -45,7 +45,7 @@ class CGradientPreview : public QWidget
     QSize sizeHint() const;
     QSize minimumSizeHint() const;
     void paintEvent(QPaintEvent *);
-    void setGrad(const GradientCont &s);
+    void setGrad(const GradientStopCont &s);
 
     public Q_SLOTS:
 
@@ -53,8 +53,8 @@ class CGradientPreview : public QWidget
 
     private:
 
-    QColor       color;
-    GradientCont stops;
+    QColor           color;
+    GradientStopCont stops;
 };
 
 class QtCurveConfig : public QWidget, private Ui::QtCurveConfigBase
@@ -129,7 +129,7 @@ class QtCurveConfig : public QWidget, private Ui::QtCurveConfigBase
     QMap<QAction *, QString> styles;
     CExportThemeDialog       *exportDialog;
     CGradientPreview         *gradPreview;
-    CustomGradientCont       customGradient;
+    GradientCont             customGradient;
     KDoubleNumInput          *shadeVals[NUM_STD_SHADES];
 };
 
