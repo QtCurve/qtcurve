b84290e3c24e75512b051c0fc694d3a40ccbe93c
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2009-03-04 20:14:51 +0000

    Fix tab fill

--- style/qtcurve.cpp
+++ style/qtcurve.cpp
@@ -4963,7 +4963,7 @@ void QtCurveStyle::drawControl(ControlElement element, const QStyleOption *optio
 
                         if(!firstTab)
                             r.adjust(0, -tabOverlap, 0, 0);
-                        fillTab(painter, r.adjusted(2, 1, 0, -1), option, fill, false, WIDGET_TAB_TOP);
+                        fillTab(painter, r.adjusted(1, 1, 0, -1), option, fill, false, WIDGET_TAB_TOP);
 
                         drawBorder(painter, r.adjusted(0, 0, 4, 0), option,
                                     selected || onlyTab
