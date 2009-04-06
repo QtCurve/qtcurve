7f931d5063a07c9833e1a33bf22716390d4ffeb7
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2009-04-06 19:16:15 +0000

    Dont alter mimestuff, etc, in user.js

--- mozilla/firefox-user.js.cmake
+++ mozilla/firefox-user.js.cmake
@@ -1,4 +1,4 @@
 user_pref("browser.preferences.instantApply", false);
-user_pref("network.protocol-handler.app.mailto", "@GTK_THEME_DIR@/mozilla/mailto.sh");
-user_pref("helpers.private_mime_types_file", "~/.mozilla/mime.types");
-user_pref("helpers.private_mailcap_file", "~/.mozilla/mailcap");
+#user_pref("network.protocol-handler.app.mailto", "@GTK_THEME_DIR@/mozilla/mailto.sh");
+#user_pref("helpers.private_mime_types_file", "~/.mozilla/mime.types");
+#user_pref("helpers.private_mailcap_file", "~/.mozilla/mailcap");
