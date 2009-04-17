62026e6d2f4b092d034e2cd32450b8e1b207d90b
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2009-04-17 20:25:51 +0000

    Fix 3d border

--- style/qtcurve.c
+++ style/qtcurve.c
@@ -1850,7 +1850,7 @@ static void drawLightBevel(cairo_t *cr, GtkStyle *style, GdkWindow *window, GtkS
                                               (MO_GLOW==opts.coloredMouseOver && WIDGET_COMBO!=widget &&
                                                !ETCH_WIDGET(widget) && WIDGET_SB_SLIDER!=widget)),
                 lightBorder=QTC_DRAW_LIGHT_BORDER(sunken, widget, app),
-                draw3dfull=!lightBorder && QTC_DRAW_3D_BORDER(sunken, app),
+                draw3dfull=!lightBorder && QTC_DRAW_3D_FULL_BORDER(sunken, app),
                 draw3d=draw3dfull || (!lightBorder && QTC_DRAW_3D_BORDER(sunken, app)),
                 bevelledButton=WIDGET_BUTTON(widget) && APPEARANCE_BEVELLED==app,
                 doEtch=flags&DF_DO_BORDER && (ETCH_WIDGET(widget) || WIDGET_COMBO_BUTTON==widget) && QTC_DO_EFFECT,
