b846d668cf6c4486f1ece2efcefe015eee92df43
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2009-06-10 21:20:51 +0000

    Move tab code out to its own source file

--- style/qtcurve.c
+++ style/qtcurve.c
@@ -58,6 +58,7 @@ static struct
 #include "qt_settings.c"
 #include "animation.c"
 #include "menu.c"
+#include "tab.c"
 #include "pixmaps.h"
 #include "config.h"
 #include <cairo.h>
@@ -174,12 +175,6 @@ static void debugDisplayWidget(GtkWidget *widget, int level)
 
 typedef struct
 {
-    int          id;
-    GdkRectangle rect;
-} QtCTab;
-
-typedef struct
-{
     GdkColor col;
     EPixmap  pix;
     double   shade;
@@ -189,7 +184,6 @@ static GtkStyleClass  *parent_class=NULL;
 static Options        opts;
 static GtkRequisition defaultOptionIndicatorSize    = { 6, 13 };
 static GtkBorder      defaultOptionIndicatorSpacing = { 7, 5, 1, 1 };
-static GHashTable     *tabHashTable                 = NULL;
 #ifdef QTC_MOUSEOVER_HANDLES
 static GHashTable     *toolbarHandleHashTable       = NULL;
 #endif
@@ -1043,124 +1037,6 @@ static void constrainRect(GdkRectangle *rect, GdkRectangle *con)
         if((rect->y+rect->height)>(con->y+con->height))
             rect->height-=(rect->y+rect->height)-(con->y+con->height);
     }
-}
-
-static QtCTab * lookupTabHash(void *hash, gboolean create)
-{
-    QtCTab *rv=NULL;
-
-    if(!tabHashTable)
-        tabHashTable=g_hash_table_new(g_direct_hash, g_direct_equal);
-
-    rv=(QtCTab *)g_hash_table_lookup(tabHashTable, hash);
-
-    if(!rv && create)
-    {
-        rv=(QtCTab *)malloc(sizeof(QtCTab));
-        rv->id=rv->rect.x=rv->rect.y=rv->rect.width=rv->rect.height=-1;
-        g_hash_table_insert(tabHashTable, hash, rv);
-        rv=g_hash_table_lookup(tabHashTable, hash);
-    }
-
-    return rv;
-}
-
-/* This function is called whenever the mouse moves within a tab - but the whole tab widget! */
-static gboolean tabEvent(GtkWidget *widget, GdkEvent *event, gpointer user_data)
-{
-    if(GDK_MOTION_NOTIFY==event->type)
-    {
-        static int last_x=-100, last_y=-100;
-
-        if(abs(last_x-event->motion.x_root)>4 || abs(last_y-event->motion.y_root)>4)
-        {
-            last_x=event->motion.x_root;
-            last_y=event->motion.y_root;
-
-            GtkNotebook *notebook=GTK_NOTEBOOK(widget);
-
-            if(notebook)
-            {
-                /* TODO! check if mouse is over tab portion! */
-                /* Find tab that mouse is currently over...*/
-                QtCTab *prevTab=lookupTabHash(widget, TRUE),
-                       currentTab;
-                int    numChildren=g_list_length(notebook->children),
-                       i,
-                       nx, ny;
-
-                currentTab.id=currentTab.rect.x=currentTab.rect.y=
-                currentTab.rect.width=currentTab.rect.height=-1;
-                gdk_window_get_origin(GTK_WIDGET(notebook)->window, &nx, &ny);
-                for (i = 0; i < numChildren; i++ )
-                {
-                    GtkWidget *page=gtk_notebook_get_nth_page(notebook, i),
-                              *tabLabel=gtk_notebook_get_tab_label(notebook, page);
-                    int       tx=(tabLabel->allocation.x+nx)-2,
-                              ty=(tabLabel->allocation.y+ny)-2,
-                              tw=(tabLabel->allocation.width)+4,
-                              th=(tabLabel->allocation.height)+4;
-
-                    if(tx<=event->motion.x_root && ty<=event->motion.y_root &&
-                       (tx+tw)>event->motion.x_root && (ty+th)>event->motion.y_root)
-                    {
-                        currentTab.rect.x=tx-nx;
-                        currentTab.rect.y=ty-ny;
-                        currentTab.rect.width=tw;
-                        currentTab.rect.height=th;
-                        currentTab.id=i;
-                        break;
-                    }
-                }
-
-                if(currentTab.id!=prevTab->id)
-                {
-                    if(currentTab.rect.x<0)
-                    {
-                        prevTab->id=currentTab.id;
-                        prevTab->rect=currentTab.rect;
-                        gtk_widget_queue_draw(widget);
-                    }
-                    else
-                    {
-                        GdkRectangle area;
-
-                        if(prevTab->rect.x<0)
-                            area=currentTab.rect;
-                        else
-                            gdk_rectangle_union(&(prevTab->rect), &(currentTab.rect), &area);
-                        prevTab->id=currentTab.id;
-                        prevTab->rect=currentTab.rect;
-                        area.x-=12;
-                        area.y-=12;
-                        area.width+=24;
-                        area.height+=24;
-                        gtk_widget_queue_draw_area(widget, area.x, area.y, area.width, area.height);
-                    }
-                }
-            }
-        }
-    }
-    else if(GDK_LEAVE_NOTIFY==event->type)
-    {
-        QtCTab *prevTab=lookupTabHash(widget, FALSE);
-
-        if(prevTab && prevTab->id>=0)
-        {
-            prevTab->id=prevTab->rect.x=prevTab->rect.y=
-            prevTab->rect.width=prevTab->rect.height=-1;
-            gtk_widget_queue_draw(widget);
-        }
-    }
-
-    return FALSE;
-}
-
-static gboolean tabDeleteEvent(GtkWidget *widget, GdkEvent *event, gpointer user_data)
-{
-    if(lookupTabHash(widget, FALSE))
-        g_hash_table_remove(tabHashTable, widget);
-    return FALSE;
 }
 
 #ifdef QTC_MOUSEOVER_HANDLES
@@ -5316,12 +5192,7 @@ debugDisplayWidget(widget, 3);
         }
 
         if(!mozTab && GTK_APP_JAVA!=qtSettings.app && !highlightTab && highlightingEnabled)
-        {
-            lookupTabHash(widget, TRUE); /* Create hash entry... */
-            gtk_widget_add_events(widget, GDK_LEAVE_NOTIFY_MASK|GDK_POINTER_MOTION_MASK);
-            g_signal_connect(G_OBJECT(widget), "unrealize", G_CALLBACK(tabDeleteEvent), widget);
-            g_signal_connect(G_OBJECT(widget), "event", G_CALLBACK(tabEvent), widget);
-        }
+            qtcTabSetup(widget);
 
 /*
         gtk_style_apply_default_background(style, window, widget && !GTK_WIDGET_NO_WINDOW(widget),
---
+++ style/tab.c
@@ -0,0 +1,211 @@
+typedef struct
+{
+    int          id;
+    GdkRectangle rect;
+} QtCTab;
+
+static GHashTable     *tabHashTable                 = NULL;
+
+static QtCTab * lookupTabHash(void *hash, gboolean create)
+{
+    QtCTab *rv=NULL;
+
+    if(!tabHashTable)
+        tabHashTable=g_hash_table_new(g_direct_hash, g_direct_equal);
+
+    rv=(QtCTab *)g_hash_table_lookup(tabHashTable, hash);
+
+    if(!rv && create)
+    {
+        rv=(QtCTab *)malloc(sizeof(QtCTab));
+        rv->id=rv->rect.x=rv->rect.y=rv->rect.width=rv->rect.height=-1;
+        g_hash_table_insert(tabHashTable, hash, rv);
+        rv=g_hash_table_lookup(tabHashTable, hash);
+    }
+
+    return rv;
+}
+
+static void qtcTabCleanup(GtkWidget *widget)
+{
+  if (widget)
+    {
+      gint id = 0;
+ 
+      id = (gint)g_object_steal_data (G_OBJECT(widget), "QTC_TAB_MOTION_ID");
+      g_signal_handler_disconnect(G_OBJECT(widget), id);
+                                             
+      id = (gint)g_object_steal_data (G_OBJECT(widget), "QTC_TAB_LEAVE_ID");
+      g_signal_handler_disconnect(G_OBJECT(widget), id);
+     
+      id = (gint)g_object_steal_data (G_OBJECT(widget), "QTC_TAB_DESTROY_ID");
+      g_signal_handler_disconnect(G_OBJECT(widget), id);
+       
+      id = (gint)g_object_steal_data (G_OBJECT(widget), "QTC_TAB_STYLE_SET_ID");
+      g_signal_handler_disconnect(G_OBJECT(widget), id);
+
+      g_object_steal_data (G_OBJECT(widget), "QTC_TAB_HACK_SET");      
+    }
+}
+
+/***********************************************
+ *   Style set signal to ensure menushell signals
+ *   get cleaned up if the theme changes
+ ***********************************************/
+static gboolean qtcTabStyleSet(GtkWidget *widget, GtkStyle *previous_style, gpointer user_data)
+{
+    qtcTabCleanup(widget);
+    return FALSE;
+}
+ 
+/***********************************************
+ *   Destroy signal to ensure menushell signals
+ *   get cleaned if it is destroyed
+ ***********************************************/
+static gboolean qtcTabDestroy(GtkWidget *widget, GdkEvent *event, gpointer user_data)
+{
+    qtcTabCleanup (widget);
+    return FALSE;
+}
+ 
+/***********************************************
+ *   Motion signal to ensure menushell items
+ *   prelight state changes on mouse move.
+ ***********************************************/
+static gboolean qtcTabMotion(GtkWidget *widget, GdkEventMotion *event, gpointer user_data)
+{
+    if (widget)
+    {
+        static int last_x=-100, last_y=-100;
+
+        if(abs(last_x-event->x_root)>4 || abs(last_y-event->y_root)>4)
+        {
+            last_x=event->x_root;
+            last_y=event->y_root;
+
+            GtkNotebook *notebook=GTK_NOTEBOOK(widget);
+
+            if(notebook)
+            {
+                /* TODO! check if mouse is over tab portion! */
+                /* Find tab that mouse is currently over...*/
+                QtCTab *prevTab=lookupTabHash(widget, TRUE),
+                       currentTab;
+                int    numChildren=g_list_length(notebook->children),
+                       i,
+                       nx, ny;
+
+                currentTab.id=currentTab.rect.x=currentTab.rect.y=
+                currentTab.rect.width=currentTab.rect.height=-1;
+                gdk_window_get_origin(GTK_WIDGET(notebook)->window, &nx, &ny);
+                for (i = 0; i < numChildren; i++ )
+                {
+                    GtkWidget *page=gtk_notebook_get_nth_page(notebook, i),
+                              *tabLabel=gtk_notebook_get_tab_label(notebook, page);
+                    int       tx=(tabLabel->allocation.x+nx)-2,
+                              ty=(tabLabel->allocation.y+ny)-2,
+                              tw=(tabLabel->allocation.width)+4,
+                              th=(tabLabel->allocation.height)+4;
+
+                    if(tx<=event->x_root && ty<=event->y_root &&
+                        (tx+tw)>event->x_root && (ty+th)>event->y_root)
+                    {
+                        currentTab.rect.x=tx-nx;
+                        currentTab.rect.y=ty-ny;
+                        currentTab.rect.width=tw;
+                        currentTab.rect.height=th;
+                        currentTab.id=i;
+                        break;
+                    }
+                }
+
+                if(currentTab.id!=prevTab->id)
+                {
+                    if(currentTab.rect.x<0)
+                    {
+                        prevTab->id=currentTab.id;
+                        prevTab->rect=currentTab.rect;
+                        gtk_widget_queue_draw(widget);
+                    }
+                    else
+                    {
+                        GdkRectangle area;
+
+                        if(prevTab->rect.x<0)
+                            area=currentTab.rect;
+                        else
+                            gdk_rectangle_union(&(prevTab->rect), &(currentTab.rect), &area);
+                        prevTab->id=currentTab.id;
+                        prevTab->rect=currentTab.rect;
+                        area.x-=12;
+                        area.y-=12;
+                        area.width+=24;
+                        area.height+=24;
+                        gtk_widget_queue_draw_area(widget, area.x, area.y, area.width, area.height);
+                    }
+                }
+            }
+        }
+    }
+ 
+    return FALSE;
+}
+ 
+/***********************************************
+ *   Leave signal to ensure menushell items
+ *   normal state on mouse leave.
+ ***********************************************/
+static gboolean qtcTabLeave(GtkWidget *widget, GdkEventCrossing *event, gpointer user_data)
+{
+  if (widget)
+    {
+        QtCTab *prevTab=lookupTabHash(widget, FALSE);
+
+        if(prevTab && prevTab->id>=0)
+        {
+            prevTab->id=prevTab->rect.x=prevTab->rect.y=
+            prevTab->rect.width=prevTab->rect.height=-1;
+            gtk_widget_queue_draw(widget);
+        }
+    }
+ 
+  return FALSE;
+}
+ 
+/***********************************************
+ *   Setup Menu Shell with signals to ensure
+ *   prelight works on items
+ ***********************************************/
+static void qtcTabSetup(GtkWidget *widget)
+{
+  if (widget)
+    {
+      gint id = 0;
+ 
+      if (!g_object_get_data(G_OBJECT(widget), "QTC_TAB_HACK_SET"))
+      {
+        id = g_signal_connect(G_OBJECT(widget), "motion-notify-event",
+                                             (GtkSignalFunc)qtcTabMotion,
+                                             NULL);
+                                  
+        g_object_set_data(G_OBJECT(widget), "QTC_TAB_MOTION_ID", (gpointer)id);
+        
+        id = g_signal_connect(G_OBJECT(widget), "leave-notify-event",
+                                             (GtkSignalFunc)qtcTabLeave,
+                                             NULL);
+        g_object_set_data(G_OBJECT(widget), "QTC_TAB_LEAVE_ID", (gpointer)id);
+                                             
+        id = g_signal_connect(G_OBJECT(widget), "destroy-event",
+                                             (GtkSignalFunc)qtcTabDestroy,
+                                             NULL);
+        g_object_set_data(G_OBJECT(widget), "QTC_TAB_DESTROY_ID", (gpointer)id);
+ 
+        g_object_set_data(G_OBJECT(widget), "QTC_TAB_HACK_SET", (gpointer)1);
+        
+        id = g_signal_connect(G_OBJECT(widget), "style-set",
+                                             (GtkSignalFunc)qtcTabStyleSet,
+                                             NULL);
+        g_object_set_data(G_OBJECT(widget), "QTC_TAB_STYLE_SET_ID", (gpointer)id);
+      }
+    }  
+}
