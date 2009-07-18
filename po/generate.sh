46e6dadc856a81ba3d99c90dd7cfcb5d60b5e72f
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2009-07-18 22:29:13 +0000

    Set charset of translations to UTF-8

--- po/generate.sh
+++ po/generate.sh
@@ -24,6 +24,11 @@ xgettext --from-code=UTF-8 -C -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki1
          -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
          --msgid-bugs-address="${BUGADDR}" \
         --files-from=infiles.list -D ${BASEDIR} -D ${WDIR} -o ${PROJECT}.pot || { echo "error while calling xgettext. aborting."; exit 1; }
+
+#Fix charset...
+cat ${PROJECT}.pot | sed s^charset=CHARSET^charset=UTF-8^g > ${PROJECT}.pot-new
+mv ${PROJECT}.pot-new ${PROJECT}.pot
+ 
 echo "Done extracting messages"
 
 echo "Merging translations"
