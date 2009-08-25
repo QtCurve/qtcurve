c1ff3f340aaac5db95ad56d714c8fb5f2e7836d3
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2009-08-25 21:16:27 +0000

    Use same shade for all menu item separators.

--- style/qtcurve.c
+++ style/qtcurve.c
@@ -4120,10 +4120,13 @@ debugDisplayWidget(widget, 3);
     }
     else if(DETAIL("hseparator"))
     {
-        int offset=opts.gtkMenuStripe && (isMozilla() || (widget && GTK_IS_MENU_ITEM(widget))) ? 20 : 0;
+        gboolean isMenuItem=widget && GTK_IS_MENU_ITEM(widget);
+
+        int offset=opts.gtkMenuStripe && (isMozilla() || isMenuItem) ? 20 : 0;
         if(offset && (GTK_APP_OPEN_OFFICE==qtSettings.app || isMozilla()))
             offset+=2;
-        drawFadedLine(cr, x+1+offset, y+(height>>1), width-(1+offset), 1, &qtcPalette.background[QT_STD_BORDER], area, NULL,
+        drawFadedLine(cr, x+1+offset, y+(height>>1), width-(1+offset), 1,
+                      &qtcPalette.background[isMenuItem ? QTC_MENU_SEP_SHADE : QT_STD_BORDER], area, NULL,
                       TRUE, TRUE, TRUE);
     }
     else if(DETAIL("vseparator"))
