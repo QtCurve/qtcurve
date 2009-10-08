5fa8accb388e8f3af245810ba873a7d390e8af49
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2009-10-08 20:15:18 +0000

    Move arrows on firefox combos 2 pixels to right

--- style/qtcurve.c
+++ style/qtcurve.c
@@ -2689,9 +2689,11 @@ debugDisplayWidget(widget, 3);
             GdkColor *arrowColor=QTC_MO_ARROW(false, &qtSettings.colors[GTK_STATE_INSENSITIVE==state
                                                                             ? PAL_DISABLED : PAL_ACTIVE]
                                                                        [COLOR_BUTTON_TEXT]);
+            gboolean moz=isMozilla() && widget && widget->parent && widget->parent->parent && widget->parent->parent->parent &&
+                         isFixedWidget(widget->parent->parent->parent);
             x++;
 
-            if(!QTC_DO_EFFECT)
+            if(!QTC_DO_EFFECT || moz)
                 x+=2;
 
             if(opts.doubleGtkComboArrow)
