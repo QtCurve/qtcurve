8105f7eaed3d517d4121c5be947ef6dea3f1643a
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2010-03-12 19:23:57 +0000

    String change

--- config/qtcurveconfig.cpp
+++ config/qtcurveconfig.cpp
@@ -537,7 +537,7 @@ static void insertStripeEntries(QComboBox *combo)
     combo->insertItem(STRIPE_NONE, i18n("Plain"));
     combo->insertItem(STRIPE_PLAIN, i18n("Striped"));
     combo->insertItem(STRIPE_DIAGONAL, i18n("Diagonal stripes"));
-    combo->insertItem(STRIPE_FADE, i18n("Fading"));
+    combo->insertItem(STRIPE_FADE, i18n("Faded stripes"));
 }
 
 static void insertSliderStyleEntries(QComboBox *combo)
