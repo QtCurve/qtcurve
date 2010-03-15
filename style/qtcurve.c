406dafcff7077ba5e6b4b36e476aa7602f250473
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2010-03-15 20:09:27 +0000

    Add option to draw agua shine on custom gradients.

--- style/qtcurve.c
+++ style/qtcurve.c
@@ -1561,6 +1561,7 @@ static void drawLightBevel(cairo_t *cr, GtkStyle *style, GtkStateType state,
                 lightBorder=!flatWidget && QTC_DRAW_LIGHT_BORDER(sunken, widget, app),
                 draw3dfull=!flatWidget && !lightBorder && QTC_DRAW_3D_FULL_BORDER(sunken, app),
                 draw3d=!flatWidget && (draw3dfull || (!lightBorder && QTC_DRAW_3D_BORDER(sunken, app))),
+                drawShine=!flatWidget && QTC_DRAW_SHINE(sunken, app),
                 bevelledButton=WIDGET_BUTTON(widget) && APPEARANCE_BEVELLED==app,
                 doEtch=flags&DF_DO_BORDER && (ETCH_WIDGET(widget) || (WIDGET_COMBO_BUTTON==widget && opts.etchEntry)) && QTC_DO_EFFECT,
                 horiz=!(flags&DF_VERT);
@@ -1673,7 +1674,7 @@ static void drawLightBevel(cairo_t *cr, GtkStyle *style, GtkStateType state,
             }
         }
         
-        if(APPEARANCE_AGUA==app && !sunken)
+        if(drawShine)
         {
             gboolean mo=GTK_STATE_PRELIGHT==state && opts.highlightFactor;
             int      xa=x, ya=y, wa=width, ha=height;
