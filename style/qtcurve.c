c239234cdb484d06c4515f04647f6c051f546376
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2010-05-25 19:08:10 +0000

    Add hidden (until 1.5.0) option to square scrollbar sliders and general sliders.

--- style/qtcurve.c
+++ style/qtcurve.c
@@ -809,11 +809,13 @@ static int getRound(const char *detail, GtkWidget *widget, int x, int y, int wid
         if(0==strcmp(detail, "slider"))
             return
 #ifndef SIMPLE_SCROLLBARS
-                    SCROLLBAR_NONE==opts.scrollbarType || opts.flatSbarButtons ? ROUNDED_ALL :
+                    !(opts.square&SQUARE_SB_SLIDER) && (SCROLLBAR_NONE==opts.scrollbarType || opts.flatSbarButtons) ? ROUNDED_ALL :
 #endif
                     ROUNDED_NONE;
-        else if(0==strcmp(detail, "qtc-slider") ||
-                0==strcmp(detail, "splitter") || 0==strcmp(detail, "optionmenu")  ||
+        else if(0==strcmp(detail, "qtc-slider"))
+            return opts.square&SQUARE_SLIDER && (SLIDER_PLAIN==opts.sliderStyle || SLIDER_PLAIN_ROTATED==opts.sliderStyle)
+                ? ROUNDED_NONE : ROUNDED_ALL;
+        else if(0==strcmp(detail, "splitter") || 0==strcmp(detail, "optionmenu")  ||
                 0==strcmp(detail, "togglebutton") || 0==strcmp(detail, "hscale") ||
                 0==strcmp(detail, "vscale") || 0==strcmp(detail, QTC_CHECKBOX)
                 /* || 0==strcmp(detail, "paned") || 0==strcmp(detail, QTC_PANED)*/ )
@@ -4240,7 +4242,7 @@ debugDisplayWidget(widget, 3);
                 wid=WIDGET_FILLED_SLIDER_TROUGH;
             }
             drawLightBevel(cr, style, state, area, NULL, x, y, width, height,
-                           bgndcol, bgndcols, ROUNDED_ALL, wid,
+                           bgndcol, bgndcols, opts.square&SQUARE_SLIDER ? ROUNDED_NONE : ROUNDED_ALL, wid,
                            BORDER_FLAT, DF_DO_CORNERS|DF_SUNKEN|DF_DO_BORDER|
                            (horiz ? 0 : DF_VERT), widget);
 
@@ -4267,7 +4269,7 @@ debugDisplayWidget(widget, 3);
                 {
                     drawLightBevel(cr, style, state, area, NULL, used_x, used_y, used_w, used_h,
                                    &usedcols[ORIGINAL_SHADE], usedcols,
-                                   ROUNDED_ALL, WIDGET_FILLED_SLIDER_TROUGH,
+                                   opts.square&SQUARE_SLIDER ? ROUNDED_NONE : ROUNDED_ALL, WIDGET_FILLED_SLIDER_TROUGH,
                                    BORDER_FLAT, DF_DO_CORNERS|DF_SUNKEN|DF_DO_BORDER|
                                    (horiz ? 0 : DF_VERT), widget);
                 }
@@ -4379,6 +4381,9 @@ debugDisplayWidget(widget, 3);
                         break;
     #endif
                 }
+
+            if(opts.square&SQUARE_SB_SLIDER)
+                sbarRound=ROUNDED_NONE;
 
             if(drawBg)
                 if(opts.gtkScrollViews && IS_FLAT(opts.sbarBgndAppearance) && 0!=opts.tabBgnd && widget && widget->parent && widget->parent->parent &&
