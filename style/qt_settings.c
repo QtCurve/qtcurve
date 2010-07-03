792b51aabe7dd1d98f52385b0d08b35d970e7e75
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2010-07-03 17:57:39 +0000

    Eclude opacity settings from Moz apps

--- style/qt_settings.c
+++ style/qt_settings.c
@@ -2261,6 +2261,9 @@ static gboolean qtInit()
             if(IMG_NONE!=opts.bgndImage.type && excludedApp(opts.noBgndImageApps))
                 opts.bgndImage.type=IMG_NONE;
 
+            if(isMozilla())
+                opts.bgndOpacity=opts.dlgOpacity=opts.menuBgndOpacity=100;
+            
             if((100!=opts.bgndOpacity || 100!=opts.dlgOpacity) && excludedApp(opts.noBgndOpacityApps))
                 opts.bgndOpacity=opts.dlgOpacity=100;
 
