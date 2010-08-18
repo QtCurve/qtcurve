350b1e5af2b712d4ccc4f06d72d600551530e041
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2010-08-18 20:30:13 +0000

    Fix alpha of agua buttons!

--- style/qtcurve.c
+++ style/qtcurve.c
@@ -1284,8 +1284,8 @@ static void drawBevelGradientAlpha(cairo_t *cr, GtkStyle *style, GdkRectangle *a
             GdkColor col;
             double pos=AGUA_MAX/((horiz ? height : width)*2.0);
             shade(&opts, base, &col, AGUA_MID_SHADE);
-            cairo_pattern_add_color_stop_rgba(pt, pos, CAIRO_COL(col), alpha*grad->stops[i].alpha);
-            cairo_pattern_add_color_stop_rgba(pt, 1.0-pos, CAIRO_COL(col), alpha*grad->stops[i].alpha);
+            cairo_pattern_add_color_stop_rgba(pt, pos, CAIRO_COL(col), alpha); /* *grad->stops[i].alpha); */
+            cairo_pattern_add_color_stop_rgba(pt, 1.0-pos, CAIRO_COL(col), alpha); /* *grad->stops[i].alpha); */
         }
 
         cairo_set_source(cr, pt);
