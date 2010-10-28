dd5ac71a0fd3d309d4c77ea5d29b5c198cf15693
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2010-10-28 18:46:15 +0000

    Fix image alignment

--- style/qtcurve.c
+++ style/qtcurve.c
@@ -2389,22 +2389,22 @@ static void drawBgndRings(cairo_t *cr, gint x, gint y, gint width, gint height, 
                         break;
                     default:
                     case PP_TR:
-                        gdk_cairo_set_source_pixbuf(cr, img->pixmap.img, x+(width-img->width), y);
+                        gdk_cairo_set_source_pixbuf(cr, img->pixmap.img, x+(width-img->width)-1, y);
                         break;
                     case PP_BL:
                         gdk_cairo_set_source_pixbuf(cr, img->pixmap.img, x, y+(height-img->height));
                         break;
                     case PP_BM:
-                        gdk_cairo_set_source_pixbuf(cr, img->pixmap.img, x+((width-img->width)/2), y+(height-img->height));
+                        gdk_cairo_set_source_pixbuf(cr, img->pixmap.img, x+((width-img->width)/2), y+(height-img->height)-1);
                         break;
                     case PP_BR:
-                        gdk_cairo_set_source_pixbuf(cr, img->pixmap.img, x+(width-img->width), y+(height-img->height));
+                        gdk_cairo_set_source_pixbuf(cr, img->pixmap.img, x+(width-img->width)-1, y+(height-img->height)-1);
                         break;
                     case PP_LM:
                         gdk_cairo_set_source_pixbuf(cr, img->pixmap.img, x, y+((height-img->height)/2));
                         break;
                     case PP_RM:
-                        gdk_cairo_set_source_pixbuf(cr, img->pixmap.img, x+(width-img->width), y+((height-img->height)/2));
+                        gdk_cairo_set_source_pixbuf(cr, img->pixmap.img, x+(width-img->width)-1, y+((height-img->height)/2));
                         break;
                     case PP_CENTRED:
                         gdk_cairo_set_source_pixbuf(cr, img->pixmap.img, x+((width-img->width)/2), y+((height-img->height)/2));
@@ -2725,9 +2725,14 @@ static gboolean drawWindowBgnd(cairo_t *cr, GtkStyle *style, GdkRectangle *area,
                 cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
 
             if(BGND_IMG_ON_BORDER)
-                wmod=(wmod/2)+1;
+            {
+                if(IMG_FILE==opts.bgndImage.type)
+                    wmod++, hmod++;
+                else
+                    wmod=(wmod/2)+1;
+            }
             else
-                wmod=0, hmod=0, ymod=0;
+                wmod=0, hmod=0, xmod=0, ymod=0;
 
             drawBgndRings(cr, -xpos+xmod, -ypos+ymod, window->allocation.width+wmod-1, window->allocation.height+hmod-1, TRUE);
             unsetCairoClipping(cr);
