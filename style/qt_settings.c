5f99a881d3b341a04c2161a11393e9155ee12b86
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2010-12-01 20:06:27 +0000

    Gtk3: Disable KDE-like non-editable combos

--- style/qt_settings.c
+++ style/qt_settings.c
@@ -2174,6 +2174,7 @@ static gboolean qtInit()
 #if GTK_CHECK_VERSION(2, 90, 0) /* Gtk3:TODO !!! */
             opts.square|=SQUARE_POPUP_MENUS;
             opts.bgndOpacity=opts.dlgOpacity=opts.menuBgndOpacity=100;
+            opts.gtkComboMenus=TRUE;
             qtSettings.useAlpha=FALSE;
 #endif
 
