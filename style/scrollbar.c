c656144d197fd49bfc791388020a52b40349e168
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2010-12-07 19:45:13 +0000

    Import

---
+++ style/scrollbar.c
@@ -0,0 +1,81 @@
+static void qtcScrollbarCleanup(GtkWidget *widget)
+{
+    if (widget && g_object_get_data(G_OBJECT(widget), "QTC_SCROLLBAR_SET"))
+    {
+        g_signal_handler_disconnect(G_OBJECT(widget),
+                                    (gint)g_object_steal_data(G_OBJECT(widget), "QTC_SCROLLBAR_DESTROY_ID"));
+        g_signal_handler_disconnect(G_OBJECT(widget),
+                                    (gint)g_object_steal_data(G_OBJECT(widget), "QTC_SCROLLBAR_UNREALIZE_ID"));
+        g_signal_handler_disconnect(G_OBJECT(widget),
+                                    (gint)g_object_steal_data(G_OBJECT(widget), "QTC_SCROLLBAR_STYLE_SET_ID"));
+        g_signal_handler_disconnect(G_OBJECT(widget),
+                                    (gint)g_object_steal_data(G_OBJECT(widget), "QTC_SCROLLBAR_VALUE_CHANGED_ID"));
+        g_object_steal_data(G_OBJECT(widget), "QTC_SCROLLBAR_SET");
+    }
+}
+
+static gboolean qtcScrollbarStyleSet(GtkWidget *widget, GtkStyle *previous_style, gpointer user_data)
+{
+    qtcScrollbarCleanup(widget);
+    return FALSE;
+}
+
+static gboolean qtcScrollbarDestroy(GtkWidget *widget, GdkEvent *event, gpointer user_data)
+{
+    qtcScrollbarCleanup(widget);
+    return FALSE;
+}
+
+static GtkScrolledWindow * qtcScrollbarParentScrolledWindow(GtkWidget *widget)
+{
+    GtkWidget *parent=widget;
+
+    while(parent && (parent=qtcWidgetGetParent(parent)))
+    {
+        if(GTK_IS_SCROLLED_WINDOW(parent))
+            return GTK_SCROLLED_WINDOW(parent);
+    }
+}
+    
+static gboolean qtcScrollbarValueChanged(GtkWidget *widget, GdkEventMotion *event, gpointer user_data)
+{
+    if(GTK_IS_SCROLLBAR(widget))
+    {
+        GtkScrolledWindow *sw=qtcScrollbarParentScrolledWindow(widget);
+
+        if(sw)
+            gtk_widget_queue_draw(GTK_WIDGET(sw));
+    }
+    return FALSE;
+}
+
+static void qtcScrollbarSetupSlider(GtkWidget *widget)
+{
+    if (widget && !g_object_get_data(G_OBJECT(widget), "QTC_SCROLLBAR_SET"))
+    {
+        g_object_set_data(G_OBJECT(widget), "QTC_SCROLLBAR_SET", (gpointer)1);
+        g_object_set_data(G_OBJECT(widget), "QTC_SCROLLBAR_DESTROY_ID",
+                          (gpointer)g_signal_connect(G_OBJECT(widget), "destroy-event", G_CALLBACK(qtcScrollbarDestroy), NULL));
+        g_object_set_data(G_OBJECT(widget), "QTC_SCROLLBAR_UNREALIZE_ID",
+                          (gpointer)g_signal_connect(G_OBJECT(widget), "unrealize", G_CALLBACK(qtcScrollbarDestroy), NULL));
+        g_object_set_data(G_OBJECT(widget), "QTC_SCROLLBAR_STYLE_SET_ID",
+                          (gpointer)g_signal_connect(G_OBJECT(widget), "style-set", G_CALLBACK(qtcScrollbarStyleSet), NULL));
+        g_object_set_data(G_OBJECT(widget), "QTC_SCROLLBAR_VALUE_CHANGED_ID",
+                          (gpointer)g_signal_connect(G_OBJECT(widget), "value-changed", G_CALLBACK(qtcScrollbarValueChanged), NULL));
+    }
+}
+
+void qtcScrollbarSetup(GtkWidget *widget)
+{
+    GtkScrolledWindow *sw=qtcScrollbarParentScrolledWindow(widget);
+
+    if(sw)
+    {
+        GtkWidget *slider;
+
+        if((slider=gtk_scrolled_window_get_hscrollbar(sw)))
+            qtcScrollbarSetupSlider(slider);
+        if((slider=gtk_scrolled_window_get_vscrollbar(sw)))
+            qtcScrollbarSetupSlider(slider);
+    }
+}
