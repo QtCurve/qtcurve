7501181db617e5977ea6747e0038e0fee98cf7d3
Yichao Yu <yyc1992@gmail.com>
2013-10-15 17:46:43 -0400

    [qt5] only initialize xcb if qt5 is running with xcb backend

--- qt5/style/qtcurve_plugin.h
+++ qt5/style/qtcurve_plugin.h
@@ -6,8 +6,7 @@
 #include "qtcurve.h"
 
 #ifdef QTC_ENABLE_X11
-#  include <QDBusConnection>
-#  include <QDBusInterface>
+#  include <QApplication>
 #  include <QX11Info>
 #  include <qtcurve-utils/x11utils.h>
 #endif
@@ -15,18 +14,17 @@
 namespace QtCurve {
 class StylePlugin: public QStylePlugin {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QStyleFactoryInterface"
-                      FILE "qtcurvestyle.json")
+    Q_PLUGIN_METADATA(IID QStyleFactoryInterface_iid FILE "qtcurvestyle.json")
 public:
-    StylePlugin(QObject *parent=0): QStylePlugin(parent) {}
-    ~StylePlugin() {}
-    virtual QStyle *create(const QString &key) override
-        {
+    virtual QStyle*
+    create(const QString &key) override
+    {
 #ifdef QTC_ENABLE_X11
+        if (qApp->platformName() == "xcb")
             qtcX11InitXcb(QX11Info::connection(), QX11Info::appScreen());
 #endif
-            return "qtcurve" == key.toLower() ? new Style : 0;
-        }
+        return "qtcurve" == key.toLower() ? new Style : 0;
+    }
 };
 }
 
