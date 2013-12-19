f1bef6fed0fadd7c57ccb03b9a3fbdcd8a41e767
Yichao Yu <yyc1992@gmail.com>
2013-12-18 19:15:41 -0500

    [macros] document

--- qtcurve-utils/macros.h
+++ qtcurve-utils/macros.h
@@ -22,28 +22,51 @@
 #ifndef _QTC_UTILS_MACROS_H_
 #define _QTC_UTILS_MACROS_H_
 
+/**
+ * Macros for detecting empty arguments, used to implement function overloading
+ * and default arguments in c.
+ * The idea of this implementation is borrowed from the following URL
+ * https://gustedt.wordpress.com/2010/06/08/detect-empty-macro-arguments/
+ * and is modified in order to fit with our usage.
+ **/
 #define __QTC_USE_3(_1, _2, _3, ...) _3
+// Test if args has one comma
 #define __QTC_HAS_COMMA1(ret_true, ret_false, args...)  \
     __QTC_USE_3(args, ret_true, ret_false)
+// Convert parentheses to comma, used to check if the next character is "("
 #define __QTC_CONVERT_PAREN(...) ,
+// Check if arg starts with "("
 #define __QTC_IS_PAREN_(ret_true, ret_false, arg)                       \
     __QTC_HAS_COMMA1(ret_true, ret_false, __QTC_CONVERT_PAREN arg)
 // Extra layer just to make sure more evaluation (if any) is done than the
 // seperator path.
 #define __QTC_IS_PAREN(ret_true, ret_false, arg)        \
     __QTC_IS_PAREN_(ret_true, ret_false, arg)
+// Check if arg is not empty and does not start with "("
+// Will not work if arg has comma or is the name of a function like macro
 #define __QTC_IS_SEP(ret_true, ret_false, arg)                          \
     __QTC_HAS_COMMA1(ret_false, ret_true, __QTC_CONVERT_PAREN arg ())
 #define __QTC_IS_EMPTY_PAREN_TRUE(ret_true, ret_false, arg) ret_false
 #define __QTC_IS_EMPTY_PAREN_FALSE(ret_true, ret_false, arg)    \
     __QTC_IS_SEP(ret_false, ret_true, arg)
-// Test if arg is empty, evaluate to ret_true if is empty,
-// evaluate to ret_false otherwise. NOTE, this may not work if arg is a macro
-// that can be evaluated to a comma separate list without parentheses
+/**
+ * Test if @arg is empty, evaluate to @ret_true if is empty,
+ * evaluate to @ret_false otherwise. NOTE, this may not work if arg is a macro
+ * that can be evaluated to a comma separate list without parentheses or is
+ * the name of a function like macro.
+ **/
 #define QTC_SWITCH(arg, ret_true, ret_false)                            \
     __QTC_IS_PAREN(__QTC_IS_EMPTY_PAREN_TRUE, __QTC_IS_EMPTY_PAREN_FALSE, arg) \
     (ret_false, ret_true, arg)
+/**
+ * Evaluate to @def if @v is empty and to @v otherwise. See #QTC_SWITCH() for
+ * restrictions.
+ **/
 #define QTC_DEFAULT(v, def) QTC_SWITCH(v, v, def)
+/**
+ * Evaluate to _##@f if @v is empty and to @f otherwise. See #QTC_SWITCH() for
+ * restrictions.
+ **/
 #define QTC_SWITCH_(v, f) QTC_SWITCH(v, f, _##f)
 
 #endif
